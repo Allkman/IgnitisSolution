35a2bcf61d6e7ed01b8f8ab65689a91bf8a9a674
algirdas.cern <algirdas.cern@gmail.com>
2025-11-03 23:05:29 +0200

    entities project

---
+++ Ignitis.Entities/PowerPlant.cs
@@ -0,0 +1,11 @@
+﻿namespace Ignitis.Entities
+{
+    public class PowerPlant
+    {
+        public Guid Id { get; set; }
+        public string Owner { get; set; } = string.Empty;
+        public decimal Power { get; set; }
+        public DateTimeOffset ValidFrom { get; set; }
+        public DateTimeOffset? ValidTo { get; set; }
+    }
+}
