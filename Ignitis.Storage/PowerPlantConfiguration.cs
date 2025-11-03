d2e7ec92e04ac66c5d5a92825c6858f4594e9102
algirdas.cern <algirdas.cern@gmail.com>
2025-11-03 23:06:12 +0200

    storage project

---
+++ Ignitis.Storage/PowerPlantConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Ignitis.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ignitis.Storage
+{
+    public class PowerPlantConfiguration : IEntityTypeConfiguration<PowerPlant>
+    {
+        public void Configure(EntityTypeBuilder<PowerPlant> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(pp => pp.Owner)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(pp => pp.Power).HasPrecision(18,1).IsRequired();
+            builder.Property(pp => pp.ValidFrom).IsRequired();
+        }
+    }
+}
---
+++ Ignitis.Storage/StorageContext.cs
@@ -0,0 +1,19 @@
+﻿using Ignitis.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ignitis.Storage
+{
+    public class StorageContext : DbContext
+    {
+        public StorageContext(DbContextOptions<StorageContext> options) : base(options) { }
+
+        public DbSet<PowerPlant> PowerPlants { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PowerPlantConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
