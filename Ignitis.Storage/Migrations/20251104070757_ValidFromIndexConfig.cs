a85083803f5b5164da69ea3a3a0b2a1a1e046c83
algirdas.cern <algirdas.cern@gmail.com>
2025-11-04 09:10:52 +0200

    validFrom index migration

---
+++ Ignitis.Storage/Migrations/20251104070757_ValidFromIndexConfig.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ignitis.Storage.Migrations
+{
+    /// <inheritdoc />
+    public partial class ValidFromIndexConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PowerPlants_ValidFrom",
+                table: "PowerPlants",
+                column: "ValidFrom");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PowerPlants_ValidFrom",
+                table: "PowerPlants");
+        }
+    }
+}
