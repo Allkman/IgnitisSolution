8ad7a6f233792096086d34d756c3a441666ea08e
algirdas.cern <algirdas.cern@gmail.com>
2025-11-09 23:43:52 +0200

    dto validation attributes

---
+++ Ignitis.Services/Attributes/NotPastDateAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ignitis.Services.Attributes
+{
+    public class NotPastDateAttribute : ValidationAttribute
+    {
+        public NotPastDateAttribute()
+        {
+            ErrorMessage = "The {0} cannot be earlier than today."; // a fallback message
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is DateTimeOffset date)
+            {
+            
+                if (date.Date < DateTimeOffset.UtcNow.Date)
+                    return new ValidationResult(string.Format(ErrorMessage!, validationContext.DisplayName));
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Ignitis.Services/Attributes/OwnerNameAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ignitis.Services.Attributes
+{
+    public class OwnerNameAttribute : ValidationAttribute
+    {
+        public OwnerNameAttribute()
+        {
+            ErrorMessage = "The {0} field must consist of first name and last name, letters only."; // a fallback message
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string owner)
+            {
+                if (string.IsNullOrWhiteSpace(owner))
+                    return new ValidationResult(ErrorMessage);
+
+                var parts = owner.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length != 2 || !parts.All(p => p.All(char.IsLetter)))
+                    return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
